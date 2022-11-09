044dd2d3c74bb93218f8fec4c4948735bbe5aaae
JeongSoo Na <skwjdtn3171@naver.com>
2022-11-09 10:53:52 +0900

    feat js

--- Basic_visualization/basic.R
+++ Basic_visualization/basic.R
@@ -2,7 +2,6 @@
 # R basic #
 ###########
 
-
 ## help
 help(summary)
 ?summary
@@ -55,3 +54,54 @@ v[2] <- "Na"
 v # other integer value change to string too
 v1 <- c(v[1],v[3])
 v1
+
+## factor
+f <- factor(c("low","high"))
+f
+## factor sequence
+f.1 <- gl(2,3, labels = c("male","female"))
+f.1
+## frequence table
+table(f.1)
+f.2 <- factor(c("white","black","white","white","black","white"))
+t <- table(f.1,f.2)
+t
+## margin.table
+margin.table(t,1) # 1:row
+margin.table(t,2) # 2:column
+## prop.table
+prop.table(t,1)
+prop.table(t,2)
+prop.table(t)
+
+## matrix
+m <- matrix(c(1,2,3,4,5,6,7,8),2,4) # row:2 / col:4
+m
+m[2,2]
+m[1,]
+m[1,-3]
+
+## array
+a <-array(1:30,dim = c(2,5,3))
+a
+
+## list
+l <- list(id = c(1,2,3), name = c("Kim","Lee","Park"), score = c(70,80,90))
+l # like json type (key & value)
+l[3]
+l[[3]]
+l$score
+unlist(l) # named string vector class
+
+## data frame
+d <- data.frame(id = c(1,2,3), name = c("Kim","Lee","Park"), score = c(70,80,90))
+d
+d$name
+d$score
+d[2,3]
+d[1,]
+d[,2]
+d[d$score >= 80,]
+d[d$name == "Kim",]
+d[d$name == "Kim",3]
+d[d$name == "Kim","score"]
